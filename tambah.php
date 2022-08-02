caced9a89f25b46f4ed66ef09b4f7ee8275a79f2
faraabishafira <faraabishafira244@gmail.com>
2022-08-02 14:53:16 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -1,9 +1,35 @@
+<?php
+require 'functions.php';
+
+$cont = mysqli_connect("localhost", "root", "", "sibp");
+
+if(isset($_POST["submit"])) {
+
+  $nis = $_POST["nis"];
+  $nama = $_POST["nama"];
+  $asrama = $_POST["asrama"];
+  $scoreawal = $_POST["score_awal"];
+  $scoreakhir = $_POST["score_akhir"];
+  $jumlah = $_POST["jumlah"];
+
+  $query = "INSERT INTO data_santri 
+              VALUES
+  ('$nis','$nama','$asrama','$scoreawal','$scoreakhir','$jumlah')";
+  mysqli_query($cont, $query);
+//cek keberasilan
+  if(mysqli_affected_rows($cont) > 0) {
+    echo "berhasil";
+  }
+}
+?>
+
 <h3>TAMBAH DATA</h3>
 
+<div class="row text-center mb-3">
 <form action="" method="post">
   <ul>
     <li>
-        <label for="nis">NIS</label>
+        <label for="nis">nis</label>
         <input type="text" name="nis" id="nis">
     </li>
     <li>
@@ -25,7 +51,11 @@
     <li>
         <label for="jumlah">jumlah</label>
         <input type="text"name="jumlah" id="jumlah">
-    </li>
+</li>
+
+<button type="submit" value=" Simpan" name="submit" >simpan</button>
     
   </ul>
 </form>
+</div>
+
