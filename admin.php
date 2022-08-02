462335d211d724ab2caba2421f0b6742a082b51f
SITI MARIA ULFA <ulfhamariamaria02@gmail.com>
2022-08-02 13:28:17 -0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+
+    <h1>Sistem informasi Bimbingan Konseling</h1>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+        
+        <!-- My Css -->
+        <link rel="stylesheet" href="style.css" />
+
+        <title>Hello, world!</title>
+        
+      </head>
+      <body id="home">
+        <!-- navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+          <div class="container">
+            <a class="navbar-brand" href="#">SIBP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#madrasah">Madrasah</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#profil">Profil</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <!-- Akhir navbar -->
+        <!-- jumbotron -->
+        <section class="jumbotron text-center">
+          <img src="img/logo.png" alt="Foto Profil" width="300" class="img-thumbnail" />
+          <h1 class="display-4">SISTEM INFORMASI BIMBINGAN KONSELING</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,96L40,90.7C80,85,160,75,240,96C320,117,400,171,480,176C560,181,640,139,720,149.3C800,160,880,224,960,229.3C1040,235,1120,181,1200,186.7C1280,192,1360,256,1400,288L1440,320L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+        </section>
+        <!-- Akhir Jumbotron -->
+        <!-- About -->
+        <section id="about">
+            <div class="container">
+                <div class="row text-center mb-3">
+                    <div class="col">
+                        <h2>Sejarah</h2>
+                    </div>
+                    <div class="row justify-content-center text-center">
+                        <div class="col-4 md-4">
+                            <p>pada tahun 1908 ketika pesantren baru dibangun oleh KHR.Syamsul Arifin masih dlam kondisi yang sunyi dan set_include_pathkarena letaknya ditengah hutan belantara. pada tahun tersebut masih merupakan awal berdirinya pondok pesantren.
+
+                            </p>
+                        </div>
+                        <div class="col-4">
+                            <p>satu dua tahun berikutnya santri mulai berdatangan, terutama dari daerah sekitar dan famili-famili KHR. Syamsul Arifin sendiri dari madura.barulah enam tahun kemudian dari rintisan awal berdirinya pondok pesantren Salafiyah Syafiiyah Sukorejo diresmikan dan dapat pengesahan langsung dari Bupati Situbondo. yaitu pada tahun 1914 M.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e2edff" fill-opacity="1" d="M0,224L40,208C80,192,160,160,240,154.7C320,149,400,171,480,154.7C560,139,640,85,720,85.3C800,85,880,139,960,170.7C1040,203,1120,213,1200,202.7C1280,192,1360,160,1400,144L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+        </section>
+        <!-- Akhir About -->
+
+                 <!-- Project -->
+<section id="projects">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>Madrasah</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 mb-6">
+                        <div class="card-body">
+                          <p class="card-text">Madrasah Ibtidaiyah salafiyah safi'iyah merupakan lembaga pendidikan yang bernaung dibawah pondok pesantren salafiyah syafiiyah sukorejo situbondo memiliki posisi yang sangat penting dalam rangka ikut serta menyiapkan kader-kader bangsa dan agama yang berkualitass dunia akhirat, 
+                            dimana latar belakang pondok pesantren salafiyah safiiyah sukorejo.
+                          </p>
+                        </div>
+                      </div>
+                  </div>
+            </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,256L40,218.7C80,181,160,107,240,96C320,85,400,139,480,160C560,181,640,171,720,144C800,117,880,75,960,85.3C1040,96,1120,160,1200,197.3C1280,235,1360,245,1400,250.7L1440,256L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+</section>
+       
+        <!-- Akhir Project -->
+        <!-- Contact -->
+        <section id="contact">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col">
+                        <h2>Contact</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <form>
+                            <div class="mb-3">
+                              <label for="name" class="form-label">Nama Lengkap</label>
+                              <input type="text" class="form-control" id="name" aria-describedby="emailHelp"/>
+                            </div>
+                            <div class="mb-3">
+                              <label for="email" class="form-label">Email</label>
+                              <input type="email" class="form-control" id="email" aria-describedby="emailHelp"/>
+                            </div>
+                            <div class="mb-3">
+                              <label for="pesan" class="form-label">Pesan</label>
+                              <textarea class="form-control" id="pesan" rows="3"></textarea>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                          </form>
+                    </div>
+                </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,256L40,245.3C80,235,160,213,240,202.7C320,192,400,192,480,197.3C560,203,640,213,720,229.3C800,245,880,267,960,256C1040,245,1120,203,1200,181.3C1280,160,1360,160,1400,160L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+        </section>
+        <!-- Akhir Contact -->
+
+        <!-- Footer -->
+        <footer class="bg-primary text-light text-center pb-5">
+            <p>Create With Love by <a href="https://www.youtube.com/c/S3TVSukorejo" class="text-white">ponpes salafiyah syafi'iyah sukorejo</a></p>
+        </footer>
+        <!-- Akhir Footer -->
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </head>
+</body>
+</html>
